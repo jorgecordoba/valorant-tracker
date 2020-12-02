9f001c1030051903cb7b955c9c5d725cd4c3ae1f
Fernando Manso <brok3r@gmail.com>
2020-12-02 21:26:48 +0100

    Weekly

--- pages/weekly.js
+++ pages/weekly.js
@@ -2,53 +2,54 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import axios from 'axios';
 import { LineGraph } from '../components/linegraph'
-import moment from 'moment'
+import { randomRGB } from '../utils/dataLayer';
 
-function startOfWeek(date) {
-    date.setHours(6, 0, 0);
-    const diff = date.getDate() - date.getDay() + 1 - 7;
-    date.setDate(diff);
-    return date.toJSON();
-}
+async function getData() {
+    const url = 'https://valorant-avg-2.azurewebsites.net/api/GetWeekStats?code=2JLGqD1qVvtZBuvxG3rzWSgX47hMPXnq6AQmjmVYeSrdp/fiOOHRUQ==';
 
-function endOfWeek(date) {
-    date.setHours(6, 0, 0);
-    var diff = date.getDate() - date.getDay() + 8 - 7;
-    date.setDate(diff);
-    return date.toJSON();
-}
-
-async function getData(from, to) {
-    
-    const url = 'https://valorant-avg-2.azurewebsites.net/api/GetMatches?code=molHze/1j3JjruEOj2/xYFZa94rlbadXqGbV1haKL2EAcXyGGwb2XQ==&from=' + from + '&to=' + to;
-    console.log(url);
     const res = await axios.get(url, {
         headers: {
             'Accept': 'application/json'
         },
     });
 
-    return res.data[0];
+    return res.data;
 }
 
 export async function getStaticProps() {
-    const from = startOfWeek(new Date());
-    const to = endOfWeek(new Date());
-    const data = await getData(from, to);
-    
-    const monMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(1, 'days'));
-    const tueMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(2, 'days'));
-    const wedMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(3, 'days'));
-    const thuMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(4, 'days'));
-    const friMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(5, 'days'));
-    const satMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(6, 'days'));
-    const sunMatches = data.map(m => moment(m.Metadata.Timestamp) < moment(from).add(7, 'days'));
-
-    
+    const data = await getData();
+    const kda = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: data.stats.map(p => {
+            const color = randomRGB();
+            return {
+                label: p.name,
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: `rgba(${color.r},${color.g},${color.b},0.4)`,
+                borderColor: `rgba(${color.r},${color.g},${color.b},1)`,
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: `rgba(${color.r},${color.g},${color.b},1)`,
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                spanGaps: true,
+                data: p.kda
+            }
+        })
+    };
 
     return {
         props: {
-            data
+            kda
         },
         revalidate: 100
     }
@@ -66,6 +67,9 @@ export default function Home(props) {
                 <h1 className={styles.title}>
                     Weekly Stats
                 </h1>
+                <div className={styles.grid}>
+                    <LineGraph data={props.kda} title='KDA Ratio' />
+                </div>
             </main>
         </div>
     )
