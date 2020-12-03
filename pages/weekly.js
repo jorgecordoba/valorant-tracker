e33ad9cb3fdfd04a9764eb22f7ff69c518a0f06c
Fernando Manso <brok3r@gmail.com>
2020-12-03 09:08:00 +0100

    Added score per round and score per round/kda

--- pages/weekly.js
+++ pages/weekly.js
@@ -16,12 +16,17 @@ async function getData() {
     return res.data;
 }
 
+const colors = {};
+
 export async function getStaticProps() {
     const data = await getData();
+
+    data.stats.forEach(s => colors[s.name] = randomRGB());
+
     const kda = {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: data.stats.map(p => {
-            const color = randomRGB();
+            const color = colors[p.name];
             return {
                 label: p.name,
                 fill: false,
@@ -47,9 +52,71 @@ export async function getStaticProps() {
         })
     };
 
+    const score = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: data.stats.map(p => {
+            const color = colors[p.name];
+            return {
+                label: p.name,
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: `rgba(${color.r},${color.g},${color.b},0.4)`,
+                borderColor: `rgba(${color.r},${color.g},${color.b},1)`,
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: `rgba(${color.r},${color.g},${color.b},1)`,
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                spanGaps: true,
+                data: p.scorePerRound
+            }
+        })
+    };
+
+    const ks = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: data.stats.map(p => {
+            const color = colors[p.name];
+            return {
+                label: p.name,
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: `rgba(${color.r},${color.g},${color.b},0.4)`,
+                borderColor: `rgba(${color.r},${color.g},${color.b},1)`,
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: `rgba(${color.r},${color.g},${color.b},1)`,
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                spanGaps: true,
+                data: p.scorePerRound.map((n,i) => n / p.kda[i])
+            }
+        })
+    };
+
+     console.log(ks);
+
     return {
         props: {
-            kda
+            kda,
+            score,
+            ks
         },
         revalidate: 100
     }
@@ -68,7 +135,9 @@ export default function Home(props) {
                     Weekly Stats
                 </h1>
                 <div className={styles.grid}>
-                    <LineGraph data={props.kda} title='KDA Ratio' />
+                    <LineGraph data={props.kda} title='KDA' />
+                    <LineGraph data={props.score} title='Score per Round' />
+                    <LineGraph data={props.ks} title='Score per Round / KDA' />
                 </div>
             </main>
         </div>
